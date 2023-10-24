9eddd4a8b52cc57bf4c04ee4ec096d7e9fa7d2ef
Manjunath.Hosamani <manjunathhosamani67@gmail.com>
2023-10-24 17:14:40 +0530

    Updated data file

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const express = require("express")
+const { products } = require("./data.js")
 const app = express()
 
+
+
 app.get("/",(req,res)=>{
-    res.json([{ name: 'Manjuanath',id: 1}, {name: 'Kumar',id: 2}])
+    res.json(products)
+  
 })
+
+app.get("/products",(req,res)=>{
+    const newProducts = products.map((product) =>{
+        const {id, name} = product;
+        return {id,name}
+
+    })
+
+    
+    // console.log(newProducts)
+    res.json(newProducts)
+})
+
+
 
 
 app.listen(5000,()=>{
